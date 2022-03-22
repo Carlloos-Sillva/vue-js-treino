904c1b367c9370645f7e784f9993a7cf0d213c1c
Carlos Eduardo da Silva <80638432+Carlloos-Sillva@users.noreply.github.com>
2022-03-22 16:05:37 -0300

    versão 01.02

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # vue-js-treino
- Construindo Single Page Applications com framework Vue.js recurso de desenvolvimento de interfaces de usuário e aplicativos de página única. Primeiro contato com vue js
+
+ Construindo Single Page Applications com framework Vue.js recurso de desenvolvimento de interfaces de usuário e aplicativos de página única. Primeiro contato com vue js.
+
+ # Estudo dentro da plataforma alura
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/App.vue
@@ -0,0 +1,72 @@
+
+<template>
+  <div class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>
+
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotos">
+
+        <meu-painel :titulo="foto.titulo">
+            <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+        </meu-painel>
+
+      </li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+import Painel from './components/shared/painel/Painel.vue';
+
+export default {
+
+  components: {
+    'meu-painel' : Painel
+  },
+
+  data() {
+
+    return {
+
+      titulo: 'Treino Vue.js', 
+      fotos: []
+    }
+  },
+
+  created() {
+
+    this.$http.get('http://localhost:3000/v1/fotos')
+      .then(res => res.json())
+      .then(fotos => this.fotos = fotos, err => console.log(err));
+  }
+}
+
+</script>
+
+<style>
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    width: 96%;
+    margin: 0 auto;
+  }
+
+  .centralizado {
+
+    text-align: center;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+
+    display: inline-block;
+  }
+
+  .imagem-responsiva {
+
+    width: 100%;
+  }
+</style>
